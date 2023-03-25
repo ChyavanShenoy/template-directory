2d105ae55c8468a264c9b7ab31b4bbc91817467f
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2023-03-26 02:41:23 +0530

    Fixed folder structure

--- dir-temp.sh
+++ dir-temp.sh
@@ -2,10 +2,21 @@
 
 read -p "Enter the folder name: " input_string
 
-subdirs=(EPT/IPT evidence data screenshots logs scans scopes tools)
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/evidence/creds
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/evidence/data
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/evidence/screenshots
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/logs
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/scans
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/scopes
+mkdir -p ~/Desktop/Projects/"${input_string}"/EPT/tools
 
-for subdir in "${subdirs[@]}"; do
-  mkdir -p ~/Desktop/Projects/"${input_string}"/"${subdir}"/creds
-done
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/evidence/creds
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/evidence/data
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/evidence/screenshots
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/logs
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/scans
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/scopes
+mkdir -p ~/Desktop/Projects/"${input_string}"/IPT/tools
 
+# Print a message to the user indicating success
 echo "Directory structure created successfully for '${input_string}'"
