4f908bb568262817f8efcb9676dcec4899cacb4a
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2023-03-26 03:00:51 +0530

    Permission prompt added to install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo mv dir-temp.sh /usr/local/bin
-sudo chmod +x /usr/local/bin/dir-temp.sh
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run with sudo. Please try again with sudo." 
+   exit 1
+fi
+
+mv dir-temp.sh /usr/local/bin
+chmod +x /usr/local/bin/dir-temp.sh
 echo -e "\033[32mdir-temp script installed successfully.\033[0m"
 echo -e "\033[34mTo run the script, type 'dir-temp' in the terminal.\033[0m"
 echo -e "\033[34mEnter the name of the project folder when prompted.\033[0m"
-
