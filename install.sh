5d93ad10501bb08cd501bc9a23f5f4cc684248c3
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2023-03-26 23:39:37 +0530

    updated permissions

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 mv dir-temp.sh /usr/local/bin/dir-temp
-chmod +x /usr/local/bin/dir-temp.sh
+chmod +x /usr/local/bin/dir-temp
 echo -e "\033[32mdir-temp script installed successfully.\033[0m"
 echo -e "\033[34mTo run the script, type 'dir-temp' in the terminal.\033[0m"
 echo -e "\033[34mEnter the name of the project folder when prompted.\033[0m"
